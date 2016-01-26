58b934c656ce4d17158061d8de32539dacb16cbf
Maic Moulinot <fulldisclosure@hotmail.fr>
2016-01-26 12:29:45 -0330

    Static Map should be defined in each DaoStatic classes

--- src/main/java/com/jomm/terroir/dao/DaoStatic.java
+++ src/main/java/com/jomm/terroir/dao/DaoStatic.java
@@ -1,15 +1,5 @@
 package com.jomm.terroir.dao;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public abstract class DaoStatic<K, E> implements Dao<K, E> {
-
-	protected Map<K, E> daoFixedMap;
-
-	public DaoStatic() {
-		if (daoFixedMap == null) {
-			daoFixedMap = new HashMap<K, E>();
-		}
-	}
+	
 }
