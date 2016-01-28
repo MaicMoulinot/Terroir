0c595d343022e557649fd8ec636bc908072718a8
Maic Moulinot <fulldisclosure@hotmail.fr>
2016-01-28 14:36:43 -0330

    Add Seller's view, UserJsf is now abstract

---
+++ src/main/java/com/jomm/terroir/web/SellerJsf.java
@@ -0,0 +1,72 @@
+package com.jomm.terroir.web;
+
+import java.util.ResourceBundle;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+
+import com.jomm.terroir.business.EnterpriseEntity;
+import com.jomm.terroir.business.SellerEntity;
+import com.jomm.terroir.business.UserEntityServiceInterface;
+import com.jomm.terroir.util.Message;
+
+@ManagedBean
+@ViewScoped
+public class SellerJsf extends UserJsf {
+	
+	// Injected fields
+	@Inject
+	private UserEntityServiceInterface userService;
+	@Inject
+	private FacesContext facesContext;
+	@Inject
+	@Message
+	private ResourceBundle resource;
+	
+	// Constants
+	private static final String USER_REGISTRED = "usersaved";
+
+	//	Attributes
+	private EnterpriseEntity enterprise;
+
+	@Override
+	public String create() {
+		userService.persistUser(convertIntoEntity());
+		FacesMessage message = new FacesMessage(resource.getString(USER_REGISTRED), null);
+		facesContext.addMessage(null, message);
+		return "sellerlist" + "?faces-redirect=true";	// Navigation case.
+	}
+
+	/**
+	 * Transform an {@link SellerJsf} into {@link SellerEntity}.
+	 * @return SellerEntity.
+	 */
+	public SellerEntity convertIntoEntity() {
+		SellerEntity userEntity = new SellerEntity();
+		userEntity.setId(getId());
+		userEntity.setFirstName(getFirstName());
+		userEntity.setLastName(getLastName());
+		userEntity.setUserName(getUserName());
+		userEntity.setEmail(getEmail());
+		userEntity.setUserPassword(getPassword());
+		userEntity.setEnterprise(getEnterprise());
+		return userEntity;
+	}
+
+	/**
+	 * @return the enterprise
+	 */
+	public EnterpriseEntity getEnterprise() {
+		return enterprise;
+	}
+
+	/**
+	 * @param enterprise the enterprise to set
+	 */
+	public void setEnterprise(EnterpriseEntity enterprise) {
+		this.enterprise = enterprise;
+	}
+}
--- src/main/java/com/jomm/terroir/web/UserJsf.java
+++ src/main/java/com/jomm/terroir/web/UserJsf.java
@@ -1,7 +1,5 @@
 package com.jomm.terroir.web;
 
-import java.util.Calendar;
-import java.util.Date;
 import java.util.ResourceBundle;
 
 import javax.faces.application.FacesMessage;
@@ -11,63 +9,37 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.inject.Inject;
 
-import com.jomm.terroir.business.CustomerEntity;
-import com.jomm.terroir.business.UserEntity;
-import com.jomm.terroir.business.UserEntityServiceInterface;
 import com.jomm.terroir.util.Message;
 
 @ManagedBean
 @ViewScoped
-public class UserJsf {
-
-	@Inject
-	private UserEntityServiceInterface userService;
+public abstract class UserJsf {
 	
+	// Injected fields
 	@Inject
 	private FacesContext facesContext;
-	
 	@Inject
 	@Message
 	private ResourceBundle resource;
-
-	//	Attributes
-	private long id;
-	private String firstName;
-	private String lastName;
-	private String userName;
-	private String email;
-	private String password;
-	private Date birthDate;
-	private boolean admin;
-	private Date signUpDate;
-
-	// Ressource bundle.
-	private static final String USER_REGISTRED = "usersaved";
+	
+	// Constants
 	private static final String PASSWORD_TITLE = "passwordtitle";
 	private static final String PASSWORD_RULES = "passwordrules";
 
-	/**
-	 * Create and save a new User.
-	 * @return "userlist" (navigation).
-	 */
-	public String create() {
-		setSignUpDate(new Date());
-		userService.persistUser(convertIntoEntity());
-		FacesMessage message = new FacesMessage(resource.getString(USER_REGISTRED), null);
-		facesContext.addMessage(null, message);
-		return "userlist" + "?faces-redirect=true";	// Navigation case.
-	}
+	//	Attributes
+	protected long id;
+	protected String firstName;
+	protected String lastName;
+	protected String userName;
+	protected String email;
+	protected String password;
+
 
 	/**
-	 * Generate the year's range for birth date calendar.
-	 * @return "minYear:maxYear"
+	 * Create and save a new User.
+	 * @return a String (navigation).
 	 */
-	public String generateYearRange() {
-		int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-		int minYear = currentYear - 97;
-		int maxYear = currentYear - 17;
-		return minYear + ":" + maxYear;
-	}
+	public abstract String create();
 
 	/**
 	 * Generate tips to create a secured enough password into growl.
@@ -76,24 +48,6 @@ public class UserJsf {
 	public void passwordTooltip(ActionEvent actionEvent) {
 		FacesMessage message = new FacesMessage(resource.getString(PASSWORD_TITLE), resource.getString(PASSWORD_RULES));
 		facesContext.addMessage("growl", message);
-	}
-
-	/**
-	 * Transform an {@link UserJsf} into {@link UserEntity}.
-	 * @return UserEntity.
-	 */
-	public UserEntity convertIntoEntity() {
-		//TODO !!
-		CustomerEntity userEntity = new CustomerEntity();
-		userEntity.setId(getId());
-		userEntity.setFirstName(getFirstName());
-		userEntity.setLastName(getLastName());
-		userEntity.setUserName(getUserName());
-		userEntity.setEmail(getEmail());
-		userEntity.setUserPassword(getPassword());
-		userEntity.setBirthDate(getBirthDate());
-		userEntity.setSignUpDate(getSignUpDate());
-		return userEntity;
 	}
 
 	/**
@@ -178,47 +132,5 @@ public class UserJsf {
 	 */
 	public void setPassword(String password) {
 		this.password = password;
-	}
-
-	/**
-	 * @return the birthDate
-	 */
-	public Date getBirthDate() {
-		return birthDate;
-	}
-
-	/**
-	 * @param birthDate the birthDate to set
-	 */
-	public void setBirthDate(Date birthDate) {
-		this.birthDate = birthDate;
-	}
-
-	/**
-	 * @return the admin
-	 */
-	public boolean isAdmin() {
-		return admin;
-	}
-
-	/**
-	 * @param admin the admin to set
-	 */
-	public void setAdmin(boolean admin) {
-		this.admin = admin;
-	}
-
-	/**
-	 * @return the signUpDate
-	 */
-	public Date getSignUpDate() {
-		return signUpDate;
-	}
-
-	/**
-	 * @param signUpDate the signUpDate to set
-	 */
-	public void setSignUpDate(Date signUpDate) {
-		this.signUpDate = signUpDate;
 	}
 }
