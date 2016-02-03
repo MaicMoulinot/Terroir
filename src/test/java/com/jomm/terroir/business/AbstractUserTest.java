9edf104c962be6ae3d2b0c5806add5bb9ea42139
Maic Moulinot <fulldisclosure@hotmail.fr>
2016-02-02 21:04:28 -0330

    Add Parameterized.class to test each child

--- src/test/java/com/jomm/terroir/business/AbstractUserTest.java
+++ src/test/java/com/jomm/terroir/business/AbstractUserTest.java
@@ -2,21 +2,49 @@ package com.jomm.terroir.business;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+
+import org.junit.After;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 /**
  * This class is a Junit test case testing the methods of {@link AbstractUser}.
+ * It is annotated {@link RunWith} {@link Parameterized} to allow the test case to run with different parameters.
+ * Here, the parameters are each child of {@link AbstractUser}.
  * @author Maic
  */
+@RunWith(Parameterized.class)
 public class AbstractUserTest {
+	
+	private AbstractUser user;
+	
+	/**
+	 * Constructor.
+	 * Its parameter comes from all values from {@link AbstractUserTest#childToTest()}.
+	 * @param class the concrete child of {@link AbstractUser}.
+	 */
+    public AbstractUserTest(AbstractUser user) {
+        this.user = user;
+    }
+    
+	/**
+	 * Reset <code>user=null</code>.
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		user = null;
+	}
 	
 	/**
 	 *  Test method for all {@link AbstractUser}'s getters and setters.
 	 */
 	@Test
 	public final void testGettersSetters() {
-		AbstractUser user = new Admin(); // AbstractUser is abstract so need to instantiate a child
-		int nb = 0;
+		long nb = 0;
 		String test = "test";
 		
 		// Id
@@ -42,7 +70,19 @@ public class AbstractUserTest {
 		// UserPassword
 		user.setUserPassword(test);
 		assertEquals("UserPassword should be " + test, test, user.getUserPassword());
-		
-		user = null; // Available for Garbage Collector
+	}
+	
+	/**
+	 * Reference a list of all {@link AbstractUser}'s concrete child to be used as parameter on constructor.
+	 * @return <code>Iterable < Object[] > </code>.
+	 */
+	@Parameters(name= "{index}: {0}")
+	public static Iterable<Object[]> childToTest() {
+		return Arrays.asList(new Object[][] {
+			{new Admin()},
+			{new Customer()},
+			{new Seller()}
+			}
+		);
 	}
 }
