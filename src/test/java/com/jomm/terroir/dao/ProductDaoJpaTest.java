5f509ebe93bf68077cd5423632bf42b4f03680d7
Maic Moulinot <fulldisclosure@hotmail.fr>
2016-02-05 19:56:10 -0330

    Junit: ProductDao

---
+++ src/test/java/com/jomm/terroir/dao/ProductDaoJpaTest.java
@@ -0,0 +1,74 @@
+package com.jomm.terroir.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ProductDaoJpaTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testGenericDao() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testCreate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testUpdate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testDelete() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testDeleteById() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testFind() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testFindAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetEntityClass() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetEntityClass() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ src/test/java/com/jomm/terroir/dao/ProductDaoTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.jomm.terroir.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Maic
+ *
+ */
+public class ProductDaoTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
