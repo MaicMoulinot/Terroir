5713fbdfcfc820c872bd984aa09cd8588941c94b
Maic Moulinot <fulldisclosure@hotmail.fr>
2016-02-14 11:35:48 -0330

    Fix bug: non transient entity

--- src/test/java/com/jomm/terroir/dao/SiteDaoJpaTest.java
+++ src/test/java/com/jomm/terroir/dao/SiteDaoJpaTest.java
@@ -11,6 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import com.jomm.terroir.business.Enterprise;
 import com.jomm.terroir.business.Site;
 import com.jomm.terroir.business.SiteTest;
 import com.jomm.terroir.util.PersistenceTest;
@@ -37,7 +38,7 @@ public class SiteDaoJpaTest extends GenericDaoTest<Site> {
 	public final void testBehavior() {
 		// EntityManager is not working, it is mocked
 		dao.setEntityManager(Mockito.mock(EntityManager.class));
-
+		dbSetupTracker.skipNextLaunch();
 		super.testBehavior();
 	}
 
@@ -46,8 +47,9 @@ public class SiteDaoJpaTest extends GenericDaoTest<Site> {
 	public final void testState() {
 		try {
 			// EntityManager is working with test-specific Persistence Unit
-			dao.setEntityManager(PersistenceTest.prepareEntityManager());
-
+			EntityManager entityManager = PersistenceTest.prepareEntityManager();
+			dao.setEntityManager(entityManager);
+			
 			Long initialId = entity.getId();
 			assertNull("Before persistence, id should be null", initialId);
 
@@ -56,6 +58,11 @@ public class SiteDaoJpaTest extends GenericDaoTest<Site> {
 			//			assertTrue("Before persistence, the list should be empty", dao.findAll().isEmpty());
 			//			assertEquals("Before persistence, the list's size should be 0", 0, dao.findAll().size());
 
+			// Retrieve an Enterprise from DataBase
+			Enterprise enterprise = findEnterpriseFromDataBase(entityManager);
+			assertNotNull("Enterprise should not be null", enterprise);
+			entity.setEnterprise(enterprise);
+			
 			// Create
 			Long persistedId = dao.create(entity).getId();
 			assertNotNull("After persistence, id should not be null", persistedId);
@@ -79,7 +86,6 @@ public class SiteDaoJpaTest extends GenericDaoTest<Site> {
 			assertNull("After DeleteById, persistedEntity should be null", dao.find(persistedId));
 
 			// Delete
-			entity = SiteTest.generateSite();
 			dao.create(entity);
 			assertNotNull("Before Delete, entity should not be null", dao.find(entity.getId()));
 			dao.delete(entity);
@@ -89,5 +95,16 @@ public class SiteDaoJpaTest extends GenericDaoTest<Site> {
 		} finally {
 			PersistenceTest.closeEntityManager();
 		}
+	}
+	
+	/**
+	 * Private method to retrieve an {@link Enterprise} from database filled with basic test data.
+	 * @param entityManager the {@link EntityManager}.
+	 * @return the {@link Enterprise} with <code>id=1</code>.
+	 */
+	private Enterprise findEnterpriseFromDataBase(EntityManager entityManager) {
+		EnterpriseDaoJpa dao = new EnterpriseDaoJpa();
+		dao.setEntityManager(entityManager);
+		return dao.find((long) 1);
 	}
 }
