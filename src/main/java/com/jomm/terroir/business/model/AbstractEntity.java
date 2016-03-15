0db1af1b0038ddec5e3fc07d0cd57f90c1936905
Maic Moulinot <fulldisclosure@hotmail.fr>
2016-03-15 15:31:28 -0230

    Javadoc: improvement class definition

--- src/main/java/com/jomm/terroir/business/model/AbstractEntity.java
+++ src/main/java/com/jomm/terroir/business/model/AbstractEntity.java
@@ -5,7 +5,10 @@ import java.io.Serializable;
 /**
  * This abstract Class describes the common methods for an {@link javax.persistence.Entity}.
  * It implements {@link Serializable} and has a default serial version ID.
- * It should be extended by all entities in the model.
+ * <br />It should be extended by all entities in the model, 
+ * and each child should fulfill the following requirement :
+ * <ul><li>it must declare a {@link javax.persistence.NamedQuery} named {@code entityName.findAll},</li>
+ * <li>its identifier must be a {@link Long}.</li></ul>
  * @author Maic
  */
 public abstract class AbstractEntity implements Serializable {
