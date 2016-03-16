2362dc717293839cac85dfbae66b1a914f4d9598
Maic Moulinot <fulldisclosure@hotmail.fr>
2016-03-16 08:21:40 -0230

    Junit: AbstractEntity

---
+++ src/test/java/com/jomm/terroir/business/model/TestAbstractEntity.java
@@ -0,0 +1,27 @@
+package com.jomm.terroir.business.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+/**
+ * This class is a Junit test case testing the methods of {@link AbstractEntity}.
+ * @author Maic
+ */
+public class TestAbstractEntity {
+
+	/**
+	 * Test method for {@link AbstractEntity#getId()}.
+	 */
+	@Test
+	public final void testGetId() {
+		Long id = (long) 33;
+		AbstractEntity entity = TestAdmin.generateAdminWithIdNull(); // a concrete child of AbstractEntity
+		assertNull(entity.getId());
+		((Admin) entity).setId(id);
+		assertNotNull(entity.getId());
+		assertEquals(id, entity.getId());
+	}
+}
