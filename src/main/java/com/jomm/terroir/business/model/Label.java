4c7dbac7611e82e8c4669f6550bd15735739118d
Maic Moulinot <fulldisclosure@hotmail.fr>
2016-03-17 09:50:26 -0230

    Label entity

---
+++ src/main/java/com/jomm/terroir/business/model/Label.java
@@ -0,0 +1,22 @@
+package com.jomm.terroir.business.model;
+
+/**
+ * @author Maic
+ *
+ */
+public class Label extends AbstractEntity {
+
+	/**
+	 * 
+	 */
+	public Label() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Long getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
