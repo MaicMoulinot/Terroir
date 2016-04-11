6a48019f79804fb811662939ecc0ca9f0cfe3418
Maic Moulinot <fulldisclosure@hotmail.fr>
2016-04-11 15:09:07 -0230

    Use AbstractEntity instead of Serializable

--- src/main/java/com/jomm/terroir/dao/Dao.java
+++ src/main/java/com/jomm/terroir/dao/Dao.java
@@ -1,57 +1,58 @@
 package com.jomm.terroir.dao;
 
-import java.io.Serializable;
 import java.util.List;
 
+import com.jomm.terroir.business.model.AbstractEntity;
+
 /**
- * This Interface describes all CRUD operations involving a {@link javax.persistence.Entity}.
+ * This Interface describes all CRUD operations involving a {@link AbstractEntity}.
  * @author Maic
  *
- * @param <E> {@link javax.persistence.Entity} is the Entity's type, which extends {@link Serializable}.
+ * @param <E> {@link AbstractEntity} is the Entity's type, which extends {@link AbstractEntity}.
  */
-public interface Dao<E extends Serializable> {
+public interface Dao<E extends AbstractEntity> {
 	
 	/**
-	 * Fetch an {@link javax.persistence.Entity}.
+	 * Fetch an {@link AbstractEntity}.
 	 * @param id {@link Long} the entity's id.
-	 * @return E the {@link javax.persistence.Entity}.
+	 * @return E the {@link AbstractEntity}.
 	 */
 	E find(Long id);
 
 	/**
-	 * Fetch all {@link javax.persistence.Entity}.
-	 * @return a list of {@link javax.persistence.Entity}.
+	 * Fetch all {@link AbstractEntity}.
+	 * @return a list of {@link AbstractEntity}.
 	 */
 	List<E> findAll();
 
 	/**
-	 * Create and persist a new {@link javax.persistence.Entity}.
-	 * @param entity the {@link javax.persistence.Entity}.
-	 * @return the persisted {@link javax.persistence.Entity}.
+	 * Create and persist a new {@link AbstractEntity}.
+	 * @param entity the {@link AbstractEntity}.
+	 * @return the persisted {@link AbstractEntity}.
 	 */
 	E create(E entity);
 
 	/**
-	 * Update a previously persisted {@link javax.persistence.Entity}.
-	 * @param entity the {@link javax.persistence.Entity}.
-	 * @return the updated {@link javax.persistence.Entity}.
+	 * Update a previously persisted {@link AbstractEntity}.
+	 * @param entity the {@link AbstractEntity}.
+	 * @return the updated {@link AbstractEntity}.
 	 */
 	E update(E entity);
 
 	/**
-	 * Delete an {@link javax.persistence.Entity}.
-	 * @param entity the {@link javax.persistence.Entity}.
+	 * Delete an {@link AbstractEntity}.
+	 * @param entity the {@link AbstractEntity}.
 	 */
 	void delete(E entity);
 
 	/**
-	 * Delete an {@link javax.persistence.Entity} using its id.
+	 * Delete an {@link AbstractEntity} using its id.
 	 * @param id {@link Long} the entity's id.
 	 */
 	void deleteById(Long id);
 	
 	/**
-	 * Get the {@link javax.persistence.Entity}'s class.
+	 * Get the {@link AbstractEntity}'s class.
 	 * @return the class of the entity.
 	 */
 	Class<E> getEntityClass();
