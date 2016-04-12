0f311451c6a1e893e25cda0b896d663e3030e1fe
Maic Moulinot <fulldisclosure@hotmail.fr>
2016-04-12 07:57:07 -0230

    AbstractEntity instead of Serializable

--- src/test/java/com/jomm/terroir/dao/TestDao.java
+++ src/test/java/com/jomm/terroir/dao/TestDao.java
@@ -3,21 +3,21 @@ package com.jomm.terroir.dao;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import java.io.Serializable;
+import com.jomm.terroir.business.model.AbstractEntity;
 
 /**
  * This abstract Class defines the contract of {@link Dao}.
  * It extends {@link UtilData} and thus has access to its {@code dbSetupTracker.skipNextLaunch()} method.
  * Each System Under Test (SUT) should have its own test class with a call to {@code super.testEntityClassMatch()}.
  * @author Maic
- * @param <E> {@link javax.persistence.Entity} is the Entity's type, which extends {@link Serializable}.
+ * @param <E> {@link javax.persistence.Entity} is the Entity's type, which extends {@link AbstractEntity}.
  */
-public abstract class TestDao<E extends Serializable> extends UtilData {
+public abstract class TestDao<E extends AbstractEntity> extends UtilData {
 	
 	// Variables //-----------------------------------------------
 	/** An implementation of {@link Dao}. */
 	protected Dao<E> dao;
-	/** A Class of {@link javax.persistence.Entity} that extends {@link Serializable}. */
+	/** A Class of {@link javax.persistence.Entity} that extends {@link AbstractEntity}. */
 	protected Class<E> entityClass;
 	
 	// Test methods //--------------------------------------------
